fcf930e1507ce43bdd9f71fa0e08a77fc099f580
ThaiLe1220 <lehongthai2000@gmail.com>
2024-02-27 19:06:27 +0700

    13

--- 13_Roman_To_Integer.py
+++ 13_Roman_To_Integer.py
@@ -9,6 +9,7 @@ L             50
 C             100
 D             500
 M             1000
+
 For example, 2 is written as II in Roman numeral, just two ones added together. 12 is written as XII, which is simply X + II. The number 27 is written as XXVII, which is XX + V + II.
 
 Roman numerals are usually written largest to smallest from left to right. However, the numeral for four is not IIII. Instead, the number four is written as IV. Because the one is before the five we subtract it making four. The same principle applies to the number nine, which is written as IX. There are six instances where subtraction is used:
@@ -21,8 +22,37 @@ Given a roman numeral, convert it to an integer.
 
 
 class Solution(object):
-    def romanToInt(self, s):
+    def romanToInt(self, s: str) -> int:
         """
         :type s: str
         :rtype: int
         """
+        roman = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
+        res = 0
+        i = 0
+
+        while i < len(s):
+            if i + 1 < len(s) and roman[s[i]] < roman[s[i + 1]]:
+                res += roman[s[i + 1]] - roman[s[i]]
+                i += 2
+
+            else:
+                res += roman[s[i]]
+                i += 1
+
+        return res
+
+
+# Test cases
+testcases = [
+    ("III", 3),
+    ("IV", 4),
+    ("IX", 9),
+    ("LVIII", 58),
+    ("MCMXCIV", 1994),
+]
+
+for testcase in testcases:
+    s, expected = testcase
+    result = Solution().romanToInt(s)
+    print(f"Input: {s}, Expected: {expected}, Output: {result}")
